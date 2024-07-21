14fa531d5f51d0ac4509c7077dffc39973a811f0
Ben Fawthrop <benjamin.fawthrop@gmail.com>
2024-07-21 12:42:06 -0400

    added src

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Benjamin Fawthrop on 7/21/24.
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+//#include <string>
+
+
+
+int main(int argc, char** argv) {
+    /*
+     * *(argv + 1) -> the # of processes to simulate
+     *                PID are given as A0, A1, A2, . . ., A9, B0, B1, . . ., Z9
+     * *(argv + 2) ->
+     *
+     * *(argv + 3) -> seed for random number gen
+     *
+     * *(argv + 4) ->
+     *
+     * *(argv + 5) -> 
+     */
+
+    string first_name = "yippee";
+
+    return 0;
+}
