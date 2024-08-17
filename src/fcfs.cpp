a60a4de2627ebdf9cbee0976ad2a209786e15671
Ben Fawthrop <benjamin.fawthrop@gmail.com>
2024-08-16 20:44:44 -0400

    added fcfs comments

--- src/fcfs.cpp
+++ src/fcfs.cpp
@@ -12,33 +12,35 @@ void fcfs::sim_and_print() {
     std::map<int, Process> io_bound_map; // stores all io bound processes keyed by their completion time
     std::priority_queue<int, std::vector<int>, std::greater<int> > io_bound_map_keys; // stores times for each io bound process completion
     bool cpu_free = true; // flag if the CPU is free to use
-//    bool io_free = true;  // flag if the I/O is free to use
     int i = 0; // makes sure all processes are added
     Process using_cpu; // process that is currently holding the CPU
-//    Process* using_io;  // process that is currently holding the I/O
     int time_cpu_frees = -1; // -1 if cpu is free, int
-    int processes_killed = 0;
+    int processes_killed = 0; // counts how many processes we've killed
 
-
+    // while there are processes alive
     while ( processes_killed < processes.size() ) {
-
         bool did_something = false; // sees if we can time skip
-
-
+        // check if we still need to add any processes and if they are in time
         if ( i < processes.size() ) {
             // adds all processes 1 at a time
-            int curr_arrival = processes[i].arrival_time;
+            int curr_arrival = processes[i].arrival_time; // arrival time of the soonest process
+            // if adding the process corresponds to current time
             if (curr_arrival >= elapsed_time && (time_cpu_frees == -1 || curr_arrival <= time_cpu_frees) &&
                 (io_bound_map_keys.empty() || curr_arrival <= io_bound_map_keys.top())) {
+
+                // adds the process
                 q.push(processes[i]);
                 elapsed_time = processes[i].arrival_time;
                 if (elapsed_time <= 9999) {
+                    // prints if allowed
                     print_line("Process " + processes[i].id + " arrived; added to the ready queue");
                 }
                 i++;
                 did_something = true;
             }
         }
+
+        // checks to see if we can send a process to CPU
         if ( cpu_free && !q.empty()) {
             cpu_free = false;
             elapsed_time += context_switch_time / 2;
@@ -46,13 +48,15 @@ void fcfs::sim_and_print() {
             time_cpu_frees = using_cpu.bursts.front() + elapsed_time;
             q.pop();
             if (elapsed_time <= 9999) {
+                // prints if allowed
                 print_line("Process " + using_cpu.id + " started using the CPU for " +
                            std::to_string(using_cpu.bursts.front()) + "ms burst");
             }
-            using_cpu.bursts.erase(using_cpu.bursts.begin());
+            using_cpu.bursts.erase(using_cpu.bursts.begin()); // remove burst just used
             did_something = true;
         }
 
+        // if we need to do another operation this iteration
         if (!did_something || time_cpu_frees == elapsed_time ||
                 (!io_bound_map_keys.empty() && io_bound_map_keys.top() == elapsed_time)) {
             if ( time_cpu_frees > 0 && time_cpu_frees <= (!io_bound_map_keys.empty() ? io_bound_map_keys.top() : time_cpu_frees) ) {
@@ -66,9 +70,6 @@ void fcfs::sim_and_print() {
                     print_line("Process " + using_cpu.id + " completed a CPU burst; " +
                             std::to_string((using_cpu.bursts.size() / 2)) + " bursts to go");
                     if (using_cpu.bursts.size() > 0) {
-                        if (elapsed_time == 3609){
-                            elapsed_time = 3609;
-                        }
                         print_line("Process " + using_cpu.id + " switching out of CPU; blocking on I/O until time " +
                                    std::to_string(elapsed_time + burst + (context_switch_time / 2)) + "ms");
                     }
