7898313f55b66a9342bc0b97249501fc82b41c0a
Ben Fawthrop <benjamin.fawthrop@gmail.com>
2024-08-18 12:29:06 -0400

    Write stats implemented for RR, everything working except wait time and turnaround for both algos;

--- src/rr.h
+++ src/rr.h
@@ -12,8 +12,8 @@ class rr {
 public:
     rr(const std::vector<Process> &processes, int context_time, int t_slc)
     : processes(processes), t_cs(context_time), elapsed_time(0), t_slc(t_slc), cpu_util(0.0),
-    cpu_wait(0.0), io_wait(0.0), cpu_turn(0.0), io_turn(0.0), num_cpu_switches(0), num_io_switches(0),
-    cpu_preempt(0), io_preempt(0){
+    cpu_wait(0.0), io_wait(0.0), cpu_turn(0.0), io_turn(0.0), cpu_bursts_in_slice(0.0), io_bursts_in_slice(0.0),
+    total_in_slice(0.0), num_cpu_switches(0), num_io_switches(0), cpu_preempt(0), io_preempt(0){
         simulate();
     }
 
@@ -25,7 +25,7 @@ private:
     std::vector<Process> processes;
     std::queue<Process> q; /* this would be a p queue for other algos */
     int t_cs, elapsed_time, t_slc;
-    double cpu_util, cpu_wait, io_wait, cpu_turn, io_turn;
+    double cpu_util, cpu_wait, io_wait, cpu_turn, io_turn, cpu_bursts_in_slice, io_bursts_in_slice, total_in_slice;
     int num_cpu_switches, num_io_switches, cpu_preempt, io_preempt;
     // helper function to get the end-of-line queue status updates
     std::string get_queue_status();
